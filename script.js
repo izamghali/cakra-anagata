3e8949e5ccd6d6ba6df9c6491ce7c78e05274b28
Izam Ghali <izam_ghali@192.168.1.16>
2023-04-09 07:41:37 +0700

    add contact links & social media icons

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.querySelector('nav').style.borderBottom = 'solid #D9D9D9';
-    document.getElementById('mainNavbar').style.height = '4em';
+    // document.getElementsByClassName('main-navbar').style.height = '4em';
     document.getElementById('brandPic').style.width = '3em';
   } else {
     document.querySelector('nav').style.borderBottom = 'solid white';
-    document.getElementById('mainNavbar').style.height = '5.5em';
+    // document.getElementsByClassName('main-navbar').style.height = '5.5em';
     document.getElementById('brandPic').style.width = '4em';
   }
 }
 
+function showMenu() {
+  // document.getElementsByClassName('navbar-collapse');
+}
+
 window.onscroll = function() {scrollFunction()};
+
+
