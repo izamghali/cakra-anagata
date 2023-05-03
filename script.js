4ee01e691a8b0342a94fd8cc26c84ea69dee8ddc
Izam Ghali <izam_ghali@192.168.1.2>
2023-05-03 07:50:31 +0700

    create wa cta sticky

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// border bottm & nav size change after scroll
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.querySelector('nav').style.borderBottom = 'solid #D9D9D9';
-    // document.getElementsByClassName('main-navbar').style.height = '4em';
     document.getElementById('brandPic').style.width = '3em';
   } else {
     document.querySelector('nav').style.borderBottom = 'solid white';
-    // document.getElementsByClassName('main-navbar').style.height = '5.5em';
     document.getElementById('brandPic').style.width = '4em';
   }
 }
+
+// const displayWhatsApp = document.getElementById('')
+
+// const timeOut = setTimeout()
 
 window.onscroll = function() {scrollFunction()};
 
